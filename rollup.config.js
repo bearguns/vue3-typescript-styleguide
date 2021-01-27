bb197692756a2978f34cce78b828b5d8fb78f20f
Sean Brage <sean.brage@teliax.com>
2021-01-27 13:04:39 -0700

    fix: output bundle css to dist

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,21 @@ import vue from "rollup-plugin-vue";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import css from "rollup-plugin-css-only";
-import scss from "rollup-plugin-scss";
+import sass from "rollup-plugin-scss";
 import copy from "rollup-plugin-copy";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: path.resolve(__dirname, "src/index.ts"),
   external: ["vue", /@babel\/runtime/],
   output: {
     format: "esm",
-    dir: "dist",
+    file: "dist/index.js",
   },
   plugins: [
-    vue({ css: false }),
+    sass(),
     css(),
-    scss(),
+    vue({ css: false }),
     babel({
       exclude: "node_modules/**",
       extensions: [".js", ".jsx", ".vue"],
